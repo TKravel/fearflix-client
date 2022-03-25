c8661ced529006429a1bfd95513017cc4393f1a8
Tim Kravel <TKravel@Yahoo.com>
2022-03-25 00:17:20 -0400

    add icons to nav, styling added

--- src/pages/home/Nav.jsx
+++ src/pages/home/Nav.jsx
@@ -1,16 +1,26 @@
+import { BellIcon } from '../../svg/BellIcon';
+import { SearchIcon } from '../../svg/SearchIcon';
+
 export const Nav = () => {
 	return (
 		<nav>
 			<ul className='left-nav'>
-				<li>FEARFLIX</li>
-				<li>Home</li>
-				<li>TV Shows</li>
-				<li>Movies</li>
-				<li>New & Popular</li>
-				<li>My List</li>
+				<li className='nav-main-li-item'>
+					<img className='main-logo' src='/fearflix.png' />
+				</li>
+				<li className='nav-li-item'>Home</li>
+				<li className='nav-li-item'>TV Shows</li>
+				<li className='nav-li-item'>Movies</li>
+				<li className='nav-li-item'>New & Popular</li>
+				<li className='nav-li-item'>My List</li>
 			</ul>
 			<ul className='right-nav'>
-				<li>Search</li>
+				<li>
+					<SearchIcon styles='nav-search-icon' />
+				</li>
+				<li>
+					<BellIcon styles='nav-alerts-icon' />
+				</li>
 				<li>Profile</li>
 			</ul>
 		</nav>
