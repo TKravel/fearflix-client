befe9c495759f12195a9443dd1436fa024d40b9d
Tim Kravel <TKravel@Yahoo.com>
2022-04-02 18:38:03 -0400

    remove attributes

--- src/svg/PlusIcon.jsx
+++ src/svg/PlusIcon.jsx
@@ -6,8 +6,6 @@ export const PlusIcon = ({ styles }) => {
 			height='24'
 			xmlns='http://www.w3.org/2000/svg'
 			viewBox='0 0 24 24'
-			fill-rule='evenodd'
-			clip-rule='evenodd'
 		>
 			<path d='M11 11v-11h1v11h11v1h-11v11h-1v-11h-11v-1h11z' />
 		</svg>
